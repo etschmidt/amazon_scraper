6360fe97c00840e6e0c51d242da28c81db4d1534
Ethan <ethan.t.schmidt@gmail.com>
2018-08-02 15:13:44 -0400

    stars

--- amazon.py
+++ amazon.py
@@ -1,30 +1,39 @@
 import requests
+import pandas
+import datetime
 from bs4 import BeautifulSoup
 
 keyword = input('Keyword: ')
+time = datetime.datetime.now().strftime('%I%M%p%d%B')
+filename = f'./{keyword}{time}.csv'
 
 url = f'https://www.amazon.com/s/?ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords={keyword}'
-
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-
 results = soup.find(id='s-results-list-atf')
 
 items = results.find_all(class_='s-result-item')
 # exclude class_='AdHolder'
 
-for x in items:
-	if x.find('h2'):
-		name = x.find('h2').get_text()
-	if x.find(class_='sx-price-large'):
-		price = x.find(class_='sx-price-large').find(class_='sx-price-whole').get_text()+'.'+x.find(class_='sx-price-large').find(class_='sx-price-fractional').get_text()
-	if x.find(class_='a-icon-star'):
-		stars = x.find(class_='a-icon-star').find(class_='a-icon-alt').get_text()[:3]
-	if x.find(class_='a-row').find('a', class_='a-size-small a-link-normal a-text-normal'):
-		reviews = x.find(class_='a-row').find('a', class_='a-size-small a-link-normal a-text-normal').get_text()
+names = []
+prices = []
+stars = []
+reviews = []
 
-	print(name)
-	print(price)
-	print(stars)
-	print(reviews)
-	print('')
+for item in items:
+
+	if item.find('h2'):
+		name = item.find('h2').get_text()
+		names.append(name)
+	if item.find(class_='sx-price-large'):
+		price = item.find(class_='sx-price-large').find(class_='sx-price-whole').get_text()+'.'+item.find(class_='sx-price-large').find(class_='sx-price-fractional').get_text()
+		prices.append(price)
+	if item.find(class_='a-icon-star'):
+		star = str(item.find(class_='a-icon-star').find(class_='a-icon-alt').get_text()[:3])
+		stars.append(star)
+	'''if item.find(class_='a-row').find('a', class_='a-size-small a-link-normal a-text-normal'):
+		review = item.find(class_='a-row').find('a', class_='a-size-small a-link-normal a-text-normal').get_text()
+		reviews.append(review)
+'''
+df = pandas.DataFrame(data={"Name": names, "Price": prices, "Stars": stars})
+df.to_csv(filename, sep=',',index=False)
